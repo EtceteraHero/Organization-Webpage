23a7a373b74f80c5f165dc6f11064eb5fe2929ad
Valdemar Etcetera <EtceteraDood@users.noreply.github.com>
2021-03-26 18:49:10 -0700

    Place holder front page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import logo from './images/cat-logo.png';  // Rotating cat logo (Logo subject to change)
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>EtceteraHero | Development</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1 className="w3-jumbo w3-animate-top">UNDER DEVELOPMENT</h1>
+          <hr className="w3-border-grey" style={{margin: 'auto', width: '40%'}} /> {/* Creates grey underline!!! */}
+          <p className="w3-large w3-center">Please check back again.</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
